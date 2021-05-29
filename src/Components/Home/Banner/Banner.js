8acb1ae2f062a50ada3d01fcc6644362baa6c06c
shahinurdev <mdshahinur6@gmail.com>
2021-05-29 17:32:49 +0600

    contact

--- src/Components/Home/Banner/Banner.js
+++ src/Components/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
            <div className="row">
                <div className="col-md-6">
                <h3>Find The Job <br />thats fits your culture</h3>
-               <input className="form-control" placeholder="Search Job " type="text" />
+               <input  className="form-control mb-sm-3" placeholder="Search Job " type="text" />
                </div>
                <div className="col-md-6">
                    <img className="rounded" src={banner} alt="" />
