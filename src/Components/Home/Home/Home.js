749dd78134f26ad52f2e04a5cfeedf63596b7532
Shahriar Emon <rd.emon2@gmail.com>
2021-05-30 10:17:34 +0600

    contact

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-<<<<<<< HEAD
-import { FeaturedCategories } from '../FeaturedCategories/FeaturedCategories';
-=======
 import Contact from '../Contact/Contact';
 import { FeaturedCategories } from '../FeaturedCategories/FeaturedCategories';
 import Footer from '../Footer/Footer';
->>>>>>> 053301ae92e1e86ae3273f468455bd27c1e3b9fd
 import Job from '../Job/Job';
 import Navbar from '../Navbar/Navbar';
 
@@ -17,11 +13,8 @@ const Home = () => {
             <Banner></Banner>
             <FeaturedCategories></FeaturedCategories>
             <Job></Job>
-<<<<<<< HEAD
-=======
             <Contact></Contact>
             <Footer></Footer>
->>>>>>> 053301ae92e1e86ae3273f468455bd27c1e3b9fd
         </div>
     );
 };
